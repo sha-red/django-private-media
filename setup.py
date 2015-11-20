9f937f6d1dd98ce14224937343e15b70fb10ccc4
Erik Stein <erik@classlibrary.net>
2015-11-20 11:52:29 +0100

    Refactored servers.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'django-private-media',
-    version = '0.1.3',
+    version = '0.1.3(b)',
     packages = find_packages(),
     include_package_data = True,
     license = 'BSD License',
