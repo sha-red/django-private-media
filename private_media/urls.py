27e38309648c3c54dd8dd5dca1d1d19ff9d7381f
Erik Stein <erik@classlibrary.net>
2017-11-03 19:38:15 +0100

    Use local import for local files.

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.conf import settings
 
-from views import serve_private_file
+from .views import serve_private_file
 
 urlpatterns = [
     url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), serve_private_file),
