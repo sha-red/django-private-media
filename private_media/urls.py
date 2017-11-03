c2bdaffb92ed0cbe48fc4a7506825797e53dd1a6
Roland van Laar <roland@micite.net>
2017-11-03 15:48:02 +0100

    Update for Django 1.10
    "patterns" is deprecated

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf import settings
 
-urlpatterns = patterns('private_media.views',
-    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
-)
+from views import serve_private_file
+
+urlpatterns = [
+    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), serve_private_file),
+]
